f64bee6d55f0467ac16f4842c351fc52c4af309e
mucahitkarakus <mocococo.80@gmail.com>
2024-01-23 14:52:19 +0300

    Add login functionality to the site

--- app.js
+++ app.js
@@ -14,15 +14,23 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   res.render('site/index')
 })
+
 app.get('/about', (req, res) => {
   res.render('site/about')
 })
+
 app.get('/blog', (req, res) => {
   res.render('site/blog')
 })
+
 app.get('/contact', (req, res) => {
   res.render('site/contact')
 })
+
+app.get('/login', (req, res) => {
+  res.render('site/login')
+})
+
 app.get('/blog-single', (req, res) => {
   res.render('site/blog-single')
 })

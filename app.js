ae206f69284e2b061d3ee3295557226e118420e2
mucahitkarakus <mocococo.80@gmail.com>
2024-01-24 17:41:23 +0300

    Update MongoDB connection string

--- app.js
+++ app.js
@@ -9,39 +9,10 @@ const hbs = exphbs.create();
 
 app.use(express.static('public'))
 
-mongoose.connect('mongodb://27017/nodeblog_test_db');
+mongoose.connect('mongodb://27017/nodeblog_db');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-app.get('/', (req, res) => {
-  res.render('site/index')
-})
-
-app.get('/about', (req, res) => {
-  res.render('site/about')
-})
-
-app.get('/blog', (req, res) => {
-  res.render('site/blog')
-})
-
-app.get('/contact', (req, res) => {
-  res.render('site/contact')
-})
-
-app.get('/blog-single', (req, res) => {
-  res.render('site/blog-single')
-})
-
-app.get('/login', (req, res) => {
-  res.render('site/login')
-})
-
-app.get('/register', (req, res) => {
-  res.render('site/register')
-})
-
 
 
 app.listen(port, hostname, () => {

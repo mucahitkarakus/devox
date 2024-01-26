8ab60f3072cdfa43f827d247558aef41c56a29cc
mucahitkarakus <mocococo.80@gmail.com>
2024-01-26 20:51:38 +0300

    Add generateDate helper function and update date format in blog.handlebars and post.handlebars

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
+const hbs = exphbs.create();
 const port = 3000;
 const hostname = "127.0.0.1";
-const hbs = exphbs.create();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const generateDate = require("./helpers/generateDate").generateDate
+
 
 mongoose.connect("mongodb://localhost:27017/nodeblog_db");
 
 app.use(fileUpload());
-
 app.use(express.static("public"));
+
+
+hbs.handlebars.registerHelper("generateDate", generateDate);
+
+
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -26,16 +32,14 @@ const myMiddleware = (req, res, next) => {
 };
 
 app.use('/', myMiddleware);
-
 app.use(bodyParser.json());
 
 const main = require("./routes/main");
 const posts = require("./routes/posts");
 
 app.use("/", main);
-
 app.use("/posts", posts);
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
